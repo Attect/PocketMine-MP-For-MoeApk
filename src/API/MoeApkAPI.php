240179ebc6a47f2695da0e44c5ffdcbfe0654939
Attect <412966721@qq.com>
2014-01-13 21:58:13 +0800

    主要API文件,核心所在

--- src/API/MoeApkAPI.php
+++ src/API/MoeApkAPI.php
@@ -1,20 +1,105 @@
 <?php
 //未完成
-define("控制台地址","http://mcpe.moeapk.com/");
-define("认证地址",控制台地址."sign");
+//最后编辑：2014年1月13日
+define("控制台地址","http://mcpe.moeapk.com/");//最好别改动这个
+define("认证地址",控制台地址."sign/");
+define("操作地址",控制台地址."run/");
 define("连接确认",控制台地址."connect");
+define("服务端私钥","ABCDEFG");//每个服务端唯一，需要领取
 
-final class MoeApk{
+final class MoeApkAPI{
     private $服务端;
-    private $响应头,$响应码;
-    private
+    private $响应头;
+    private $响应码=Array();
+    private $运行状态=0; //0是载入中，1是运行中
+    private $控制台公匙;
+    private $上次报告时间=0;
+    private $服务端状态=false;
+
+    public $时间计数=0;
 
     function __construct(){
         $this->服务端 = ServerAPI::request();
+        $this->连接();
+        $this->获取公匙();
+        $this->核对公匙();
+
+        $this->服务端->api->console->register("moeapk-status", "", array($this, "命令处理器"));
+        $this->服务端->api->console->register("moeapk-active", "", array($this, "命令处理器"));
+        console("[INFO] MoeApk控制台API已经成功载入");
+    }
+
+    public function 命令处理($命令,$参数,$身份,$别名){
+        if($身份!="rcon"){
+            $this->服务端->api->ban->kick($身份,"非法使用控制台命令");
+        }
+        switch($命令){
+            case "moeapk-status":
+                console("[INFO] 本次公匙：".$this->控制台公匙);
+                console("[INFO] 上次报告：".(date("U")-$this->上次报告时间)."秒前");
+                if($this->服务端状态){
+                    console("[INFO] 控制台已经被控制台激活");
+                }else{
+                    console("[ERROR] 控制台尚未激活");
+                }
+                break;
+            case "moeapk-active":
+                $this->激活服务端();
+                console("[INFO] 服务端已被控制台激活");
+                break;
+
+        }
     }
 
     public function 连接($地址=连接确认){
-        switch($this->获得状态码($地址)){
+        $this->错误码处理($this->获得状态码($地址));
+    }
+
+    public function 获取($地址){
+        return file_get_contents($地址);
+    }
+
+    public function 存活报告(){
+        $this->连接(操作地址."keep_".$this->控制台公匙."_".count($this->服务端->clients));
+    }
+
+    public function 允许玩家($玩家名){
+        if(!$this->服务端状态){
+            console("[ERROR] 服务端未被控制台激活就有人尝试入服");
+            $this->退出();
+        }
+        switch($this->获取(操作地址."playerin_".$this->控制台公匙."_".$玩家名)){
+            case "200":
+                console("[INFO] 玩家 ".$玩家名." 已成功通过控制台许可");
+                return true;
+                break;
+            case "403":
+                console("[INFO] 玩家 ".$玩家名." 被控制台阻止进入服务器");
+                return false;
+                break;
+            case "404":
+                console("[INFO] 玩家 ".$玩家名." 没有在控制台注册");
+                return false;
+                break;
+            default:
+                console("[INFO] 玩家 ".$玩家名." 进服失败，控制台返回异常");
+                return false;
+                break;
+        }
+    }
+
+    private function 获取公匙(){
+        $this->错误码处理($this->获得状态码(认证地址.服务端私钥."_".urlencode($this->服务端->name)."_".$this->服务端->maxClients."_".$this->服务端->port."_".$this->服务端->api->getProperty("rcon.password")));
+        $this->控制台公匙=$this->获取(认证地址.服务端私钥."_".urlencode($this->服务端->name)."_".$this->服务端->maxClients."_".$this->服务端->port."_".$this->服务端->api->getProperty("rcon.password"));
+        console("[DEBUG] 成功取得本次控制台公匙：".$this->控制台公匙);
+    }
+
+    private function 核对公匙(){
+        $this->连接(操作地址."checksn_".$this->控制台公匙);
+    }
+
+    private function 错误码处理($响应代码){
+        switch($响应代码){
             case "200":
                 console("[INFO] 控制台响应正常");
                 return true;
@@ -23,16 +108,44 @@ final class MoeApk{
             case "503":
             case "504":
                 console("[ERROR] 控制台服务器发生错误");
-                return false;
+                $this->退出();
+                break;
+            case "403":
+                console("[ERROR] 控制台拒绝了连接请求");
+                $this->退出();
+                break;
+            case "404":
+                console("[ERROR] 控制台响应所请求的事件不存在");
+                $this->退出();
+                break;
         }
+        console("[ERROR] 服务端返回不正常状态");
+        $this->退出();
     }
 
 
 
     private function 获得状态码($地址){
         $this->响应头=get_headers($地址);
-        preg_match("/HTTP\/1.1 (.+?) OK/",$this->响应头,$this->响应码);
+        preg_match('/HTTP\/1.1 (.+?) /',$this->响应头[0],$this->响应码);
+        console("[DEBUG] ".$地址);
         return $this->响应码[1];
     }
+
+    private function 激活服务端(){
+        $this->服务端状态=true;
+    }
+
+    private function 退出(){
+        console("[INFO] 服务端开始退出");
+        switch($this->运行状态){
+            case 0:
+            case 1:
+                $this->服务端->close();//标准退出防止存档损坏
+        }
+        kill(getmypid());//强制退出防止终端卡住
+    }
+
+
 }
 ?>
